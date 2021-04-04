7ed0930b1dfaa435c806ef3ef26745b129b98d09
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-04-04 18:14:17 +0900

    safer implementation

--- auto/auto.py
+++ auto/auto.py
@@ -45,8 +45,6 @@ class CasedFunction:
             key = self.default_key
         return self.funcs[key]
 
-# TODO: max_n_file using len(self.hist)
-
 class AutoAnalyzer:
     def __init__(self, path, recursive=False, include=None, exclude=None, 
                  dt=10, t_end=10*60*60, n_files=100):
@@ -57,18 +55,23 @@ class AutoAnalyzer:
             Path to the directory that will contain files for analysis.
         recursive : bool, optional
             If recursively search for files, by default False
-        filename : str, optional
-            # Filename to analyze. You can also use wildcard "*" (e.g. if `filename` 
-            # is "*-nM-Protein.*" then both "10-nM-Protein.tif" and "50-nM-Protein.png"
-            # will match but "DNA.txt" will not).
-            # This paramter is very important when input files and output files have the 
-            # same extension (e.g. load a tif file, apply median filter, and save it as 
-            # tif file) because the newly saved file will be analyzed again.
-            # By default "*".
+        include : str, optional [^1]
+            Only files whose name matches this regular expression will be analyzed.
+        exclude : str, optional [^1]
+            All the files whose name matches this regular expression will be ignored.
         dt : float, optional
-            All the files will be scanned every `dt` seconds, by default 10 hours.
+            All the files will be scanned every `dt` seconds, by default 10 seconds.
         t_end : float, optional
-            After `t_end` seconds this program will stop, by default 10800.
+            After `t_end` seconds this program will stop, by default 10 hours.
+        n_files : int, optional [^2]
+            After `n_files` files are analyzed this program will stop, by default
+            100 files.
+        
+        [^1]: These paramters are very important when input files and output files have
+        the same extension (e.g. load a tif file, apply median filter, and save it as 
+        tif file) because the newly saved file will be analyzed again.
+        [^2]: This parameter is also important in case `include` and `exclude` are set
+        inappropriately. n_files will be a `stopper` on this accident.
         """        
         
         self.path = path
@@ -170,19 +173,24 @@ class AutoAnalyzer:
     def _run_loop(self):
         self._add_log("{} | start scanning.".format(now()))
         t0 = time.time()
-        while self.loop and time.time() - t0 < self.t_end:
+        
+        while self.loop:
             for f in self.scan():
                 (f not in self.hist) and self._run(f)
-            time.sleep(self.dt)
             
-            if time.time() - t0 < self.t_end:
-                print("Analysis time exceeded {} sec.".format(self.t_end))
-                print("For longer analysis, set t_end to larger value (10 hr by default).")
-                self.loop = False
-            if len(self.hist) > self.n_files:
-                print("Number of analyzed files exceeded {}".format(self.n_files))
-                print("For larger number, set n_files to larger value (100 by default).")
-                self.loop = False
+                if time.time() - t0 > self.t_end:
+                    print("Analysis time exceeded {} sec.".format(self.t_end))
+                    print("For longer analysis, set t_end to larger value (10 hr by default).")
+                    self.loop = False
+                    break
+                    
+                if len(self.hist) > self.n_files:
+                    print("Number of analyzed files exceeded {}".format(self.n_files))
+                    print("For larger number, set n_files to larger value (100 by default).")
+                    self.loop = False
+                    break
+            
+            time.sleep(self.dt)    
         
         return None    
     
